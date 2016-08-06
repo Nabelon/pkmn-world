dd260a7a315f28942665eba269c3c63f0a7b7dd1
Bas Tuijnman <bas.tuijnman@24i.com>
2016-08-06 22:47:40 +0200

    Add monster behaviour for selectable monsters on the map

---
+++ Assets/Scripts/Map/Monster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+    void Update() {
+
+        // Check for left mouse button
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) {
+            Vector2 position;
+
+			if (Input.touchSupported) {
+
+				// Use touch position when supported by the device
+				position = Input.GetTouch (0).position;
+			} else {
+
+				// Use mouse position as a fallback
+				position = Input.mousePosition;
+			}
+
+			Ray ray = Camera.main.ScreenPointToRay (position);
+			RaycastHit hit;
+
+			// Do a raycast, the max size here needs to be better defined.
+			if (Physics.Raycast (ray, out hit, 300.0f)) {
+				if (hit.transform == transform) {
+
+					// TODO: Move to the battle scene
+
+				}
+			}
+        }
+
+    }
+
+}
